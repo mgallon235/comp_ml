5e21bdef82239a6d955bd2ddb2df6e8fd4830be8
mgallon235 <125981129+mgallon235@users.noreply.github.com>
2023-10-18 12:20:42 +0200

    update

---
+++ TA_session1.py
@@ -0,0 +1,13 @@
+# Missing Values
+
+#1. Deletion
+#listwise : deletes the row with at least 1 missing values
+#pairwise : deletes rows with missing values based on a specif columns
+
+#2. Imputation
+#mean
+#fill_forward / backwards
+#linear interpolation - will input a value based on a linear relationship between the closest values / linear
+#KNN - input value based on the closest values / non-linear
+
+#3. 
